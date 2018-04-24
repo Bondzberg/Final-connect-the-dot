c6dc2b71bb989e13d24031dc9a44639ef5e04d0c
Bondzberg <32276521+Bondzberg@users.noreply.github.com>
2018-04-24 15:07:48 -0500

    f

--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,7 @@
 import mayflower.Mayflower;
 import mayflower.World;
+import mayflower.event.EventListener;
+import mayflower.ui.Button;
 
 public class Runner extends Mayflower
 {
@@ -11,6 +13,7 @@ public class Runner extends Mayflower
     }
     public void init()
     {
+
         Mayflower.setWorld(new World() {
             @Override
             public void act() {
@@ -20,6 +23,6 @@ public class Runner extends Mayflower
     }
     public void main()
     {
-        System.out.println();
+
     }
 }
