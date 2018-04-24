d73ca599fb658a1f37302007ae5280b0b85d650b
Bondzberg <32276521+Bondzberg@users.noreply.github.com>
2018-04-24 15:46:09 -0500

    some Game stuff

--- src/GameWorld.java
+++ src/GameWorld.java
@@ -1,24 +1,62 @@
+import mayflower.World;
 import mayflower.event.EventListener;
 
-public abstract class GameWorld implements EventListener
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public abstract class GameWorld extends World implements EventListener
 {
     private int[][] squares;
     private Dot[][] Dots;
-    public GameWorld()
+    private HashMap<Dot,List<Integer>> dts;
+    public GameWorld(int x,int y)
     {
-        Dots = new Dot[5][5];
-        for(Dot[] dots1: Dots)
+        Dots = new Dot[x][y];
+        squares = new int[x-1][y-1];
+        dts = new HashMap();
+        for(int c=0;c<y;c++)
         {
-            for(Dot dot:dots1)
+            for(int r = 0;r<x;r++)
             {
+                Dots[c][r]=new Dot("placeholder",x+" "+y);
+                Dots[c][r].addEventListener(this);
+                addObject(Dots[c][r],c*10,r*10);
 
             }
         }
-        
+
+        for(int c=0;c<squares.length;c++)
+        {
+            for(int r = 0;r<squares[0].length;r++)
+            {
+
+                squares[c][r] = 0;
+                dts.get(Dots[c][r]).add(squares[c][r]);
+                dts.get(Dots[c+1][r]).add(squares[c][r]);
+                dts.get(Dots[c][r+1]).add(squares[c][r]);
+                dts.get(Dots[c+1][r+1]).add(squares[c][r]);
+
+            }
+        }
+
+
     }
     @Override
     public void onEvent(String s)
     {
+        String[] cord = s.split(" ");
+        int x = Integer.valueOf(cord[0]);
+        int y = Integer.valueOf(cord[1]);
+        List<Integer> i = dts.get(Dots[x][y]);
+        for(int l:i)
+        {
+            l++;
+        }
+    }
+
+    @Override
+    public void act() {
 
     }
 }
