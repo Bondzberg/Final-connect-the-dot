595ad5f4a6ce1330950024133d528f36d4d206f1
Zach Spelce <zbspelce@gmail.com>
2018-04-26 16:15:52 -0500

    Map Stats

--- src/Profile.java
+++ src/Profile.java
@@ -2,41 +2,21 @@ import mayflower.Color;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Profile
 {
     private String name;
-    private int wins;
-    private int losses;
-    private int ties;
-    private Color color;
+    private Stats s;
 
-    public Profile()
+    public Profile(Map<String, Stats> s)
     {
-        name = null;
-        wins = 0;
-        losses = 0;
-        ties = 0;
-        color = null;
+
     }
 
-    public Profile(String name)
+    public Profile(Map<String, Stats> s, String name)
     {
-        boolean found = false;
-        try
-        {
-            Scanner s = new Scanner(new File("stats.txt"));
-            while(s.hasNextLine() && found == false)
-            {
-
-            }
-        }
-        catch(FileNotFoundException e)
-        {
-            e.printStackTrace();
-        }
-        this.name = name;
 
     }
 
