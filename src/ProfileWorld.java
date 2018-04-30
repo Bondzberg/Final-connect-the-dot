823a015523fe72fef8369b949c6501212ae3965e
Zach Spelce <zbspelce@gmail.com>
2018-04-30 16:09:37 -0500

    Profile Done

--- src/ProfileWorld.java
+++ src/ProfileWorld.java
@@ -20,10 +20,11 @@ public class ProfileWorld extends World implements EventListener
         back.addEventListener(this);
         back.getImage().scale(0.8);
         addObject(back, 300, 400);
+        showText("Back", 360, 455);
         showText("Wins: " + s.getWins(), 100, 100);
         showText("Losses: " + s.getLosses(), 100, 175);
         showText("Ties: " + s.getTies(), 100, 250);
-        showText("Color: " + s.getColor(), 100, 325);
+        showText("Color: " + s.colorToString(), 100, 325);
     }
 
     public void onEvent(String s)
