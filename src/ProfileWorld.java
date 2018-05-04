25789ec9ff54a5e6fbe928180f3de76bf8a643d4
Zach Spelce <zbspelce@gmail.com>
2018-05-04 15:32:28 -0500

    Color Switcher

--- src/ProfileWorld.java
+++ src/ProfileWorld.java
@@ -4,13 +4,17 @@ import mayflower.event.EventListener;
 import mayflower.ui.Button;
 import org.w3c.dom.css.Rect;
 
+import javax.swing.*;
 import java.awt.*;
 import java.util.Map;
 
 public class ProfileWorld extends StatsWorld implements EventListener
 {
     private Button back;
+    private Button colorButton;
     private MenuWorld world;
+
+    private Actor img;
 
     public ProfileWorld(MenuWorld world)
     {
@@ -19,12 +23,17 @@ public class ProfileWorld extends StatsWorld implements EventListener
         back = new Button("imgs/Button.png", "back");
         back.addEventListener(this);
         back.getImage().scale(0.8);
+        colorButton = new Button("imgs/Button.png", "color");
+        colorButton.addEventListener(this);
+        colorButton.getImage().scale(.5);
         addObject(back, 300, 400);
+        addObject(colorButton, 260, 290);
         showText("Back", 360, 455);
         showText("Wins: " + StatsWorld.playerStats.getWins(), 100, 100);
         showText("Losses: " + StatsWorld.playerStats.getLosses(), 100, 175);
         showText("Ties: " + StatsWorld.playerStats.getTies(), 100, 250);
         showText("Color: ", 100, 325);
+        showText("Change Color", 15, 275, 322);
         MayflowerImage square = new MayflowerImage("imgs/squares.png");
         for(int r = 0;r<square.getWidth();r++)
         {
@@ -35,7 +44,7 @@ public class ProfileWorld extends StatsWorld implements EventListener
                 square.setColorAt(r,c,StatsWorld.playerStats.getColor());
             }
         }
-        Actor img = new Actor(){
+        img = new Actor(){
             public void act(){}
         };
         img.setImage(square);
@@ -48,12 +57,31 @@ public class ProfileWorld extends StatsWorld implements EventListener
         {
             Mayflower.setWorld(world);
         }
+        else if(s.equals("color"))
+        {
+            java.awt.Color colour = JColorChooser.showDialog(new JPanel(),"something", java.awt.Color.BLUE);
+            StatsWorld.playerStats.setColor(new Color(colour.getRed(),colour.getGreen(),colour.getBlue()));
+            updateColor();
+        }
     }
 
     public void act()
     {
 
     }
-    /*java.awt.Color colour = JColorChooser.showDialog(new JPanel(),"something", java.awt.Color.BLUE);
-        color = new Color(colour.getBlue(),colour.getRed(),colour.getGreen());*/
+
+    public void updateColor()
+    {
+        MayflowerImage square = new MayflowerImage("imgs/squares.png");
+        for(int r = 0;r<square.getWidth();r++)
+        {
+
+            for(int c =0;c<square.getHeight();c++)
+            {
+                square.setColorAt(r,c,StatsWorld.playerStats.getColor());
+            }
+        }
+        img.setImage(square);
+    }
+
 }
